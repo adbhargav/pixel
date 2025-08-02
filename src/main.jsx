3fbf374d80aea90db89842d12f83892038f8ab94
BHARGAV A <bhargavandhe202@gmail.com>
2025-08-02 21:10:40 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
+// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // ✅ Import Tailwind CSS
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
